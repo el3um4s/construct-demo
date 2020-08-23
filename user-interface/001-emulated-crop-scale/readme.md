54a0c9a601e1e1e0fed0430640405cadc05970fc
Samuele De Tomasi <el3um4s@gmail.com>
2020-08-23 23:36:22 +0200

    update "emulated crop scale"
    Added js version

--- user-interface/001-emulated-crop-scale/readme.md
+++ user-interface/001-emulated-crop-scale/readme.md
@@ -1,5 +1,6 @@
 # Emulated Crop Scale
 
-* **c3p** [Emulated Crop Scale-20.08.20.c3p](source/c3p/emulated-crop-scale-20.08.20.c3p)
+* **c3p - only event sheet** [Emulated Crop Scale-20.08.20.c3p](source/c3p/emulated-crop-scale-20.08.20.c3p)
+* **c3p - with js** [Emulated Crop Scale-20.08.23.c3p](source/c3p/emulated-crop-scale-20.08.23.c3p)
 * **demo** [link](demo)
 
--- user-interface/001-emulated-crop-scale/source/files/scripts/runonstartup.js
+++ user-interface/001-emulated-crop-scale/source/files/scripts/runonstartup.js
@@ -1,2 +1,18 @@
 runOnStartup(async  runtime =>  {  globalThis.g_runtime  =  runtime })
 function waitForMillisecond(ms) { return new Promise(res => setTimeout(res, ms)); }
+
+function onResized() {
+	console.log("resized");
+	g_runtime.callFunction("onResized");
+}
+
+function onFullScreen() {
+	console.log("full screen");
+	setTimeout(function(){ g_runtime.callFunction("onResized"); }, 10);
+}
+
+runOnStartup(async runtime =>
+{
+	window.addEventListener("resize", () => onResized());
+	window.addEventListener("fullscreenchange", () => onFullScreen());
+});
