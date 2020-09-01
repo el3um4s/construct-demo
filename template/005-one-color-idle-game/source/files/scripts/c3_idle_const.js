3893a2c7c1bff878402dcfe7dea2589e6b2e4881
Samuele De Tomasi <el3um4s@gmail.com>
2020-09-02 00:40:28 +0200

    one color idle game: new json file: stats

---
+++ template/005-one-color-idle-game/source/files/scripts/c3_idle_const.js
@@ -0,0 +1,2 @@
+const idle_rules_JSON = "idle_rules";
+const idle_stats_JSON = "idle_stats";
--- template/005-one-color-idle-game/source/files/scripts/c3_idle_count_helper.js
+++ template/005-one-color-idle-game/source/files/scripts/c3_idle_count_helper.js
@@ -3,3 +3,8 @@ TO DO:
 	- singleGenerator
 	- totalGenerator
 */
+
+
+function calculateSingleGeneratorIncome(nameGenerator) {
+	const baseIncome_Array = json_getKey(idle_rules_JSON, `generators.${nameGenerator}.effects`);
+}
