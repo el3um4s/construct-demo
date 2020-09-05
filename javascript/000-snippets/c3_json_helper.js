86b9e035991d6808e596e15c57f91519537a47cd
Samuele de Tomasi <el3um4s@gmail.com>
2020-09-05 18:19:01 +0200

    C3 JSON HELPER: bugfix

--- javascript/000-snippets/c3_json_helper.js
+++ javascript/000-snippets/c3_json_helper.js
@@ -6,7 +6,7 @@
  * https://opensource.org/licenses/MIT
  *
  * Github:  https://github.com/el3um4s/construct-demo
- * Version: 20.07.17
+ * Version: 20.09.05
  *
 */
 
@@ -18,27 +18,58 @@ function json_initialize(json_name) {
 	jsonObj[json_name] = json[json_name].getJsonDataCopy();
 }
 
-function json_getKey(json_name, json_key){
+function json_getKey(json_name, json_key, value_default = 0){
 	const obj = jsonObj[json_name];
-	const result = json_key.split('.').reduce((o,i)=>o[i], obj);
+	
+	let result = value_default;
+	try {
+		result = json_key.split('.').reduce((o,i)=>o[i], obj);
+	} catch (e) {
+		json_changeKey(json_name, json_key, value_default);
+	}
 	return result;
 }
 
-function json_changeKey(json_name, key, value) {
 
-	function index(obj, is, value) {
-    	if (typeof is == 'string')
-        	return index(obj,is.split('.'), value);
-    	else if (is.length==1 && value!==undefined)
-        	return obj[is[0]] = value;
-    	else if (is.length==0)
-        	return obj;
-    	else
-        	return index(obj[is[0]],is.slice(1), value);
+function json_changeKey(json_name, json_key, value) {
+	
+	function assegnaValore(objOriginal, key, value) {
+  		const clone = { ...objOriginal }
+  		clone[key] = value;
+  		return clone;
+	};
+	
+	function esisteLaChiave(objOriginal, key) {
+   		return key in objOriginal;
+	};
+	
+	function assegnaChiave(objOriginal, key, value) {
+		const clone = { ...objOriginal }
+		const keyLength = key.length;
+		let result = {};
+
+		if (keyLength == 1) {
+		  const keyName = key[0];     
+		  result = {...clone, ...assegnaValore(clone, keyName, value)};
+		} else if (esisteLaChiave(clone, key[0])) {
+		  const newKey = key.slice(1);
+		  const keyName = key[0];
+		  const newClone = {};
+		  newClone[keyName] = assegnaChiave(clone[keyName], newKey, value)
+		  result = {...clone, ...newClone};
+		} else {
+		  const newKey = key.slice(1);
+		  const keyName = key[0]; 
+		  const newClone = {}
+		  newClone[keyName] = assegnaChiave({}, newKey, value);
+		  result = {...clone, ...newClone};
+		}
+	  return result
 	}
 	
 	const obj = jsonObj[json_name];
-	const result = index(obj, key, value);
-	json[json_name].setJsonDataCopy(obj);
+	const key = json_key.split('.')
+	const result = assegnaChiave(obj, key, value);
+	json[json_name].setJsonDataCopy(result);
 	return result;
 }
--- template/005-one-color-idle-game/source/files/scripts/c3_idle_count_helper.js
+++ template/005-one-color-idle-game/source/files/scripts/c3_idle_count_helper.js
@@ -23,6 +23,18 @@ function createTemporaryObject(arrayKeys) {
 	return result;
 }
 
+function addObjPropertyToProperty(objOriginal, objWithData) {
+	const clone = { ...objOriginal };
+	Object.keys(objWithData).forEach( k => clone[k] += parseFloat(objWithData[k]));
+	return clone
+}
+
+function multipleObjPropertyToProperty(objOriginal, x) {
+	const clone = { ...objOriginal };
+	Object.keys(objOriginal).forEach( k => clone[k] *= parseFloat(x));
+	return clone
+}
+
 function isCurrencyInStats(idle_stats_JSON, currency) {
 	return Object.keys(idle_stats_JSON.currencies).includes(currency);
 }
@@ -30,29 +42,104 @@ function isCurrencyInStats(idle_stats_JSON, currency) {
 
 function calculateIncome(quantity = 0, { currency = "primary", base_income = "1"} = {}) {
 	const obj = {};
-	return obj[currency] = quantity * parseFloat(base_income);
+	const result = quantity * parseFloat(base_income);
+	return obj[currency] = result;
 }
 
 function typeOfGeneratorEffect({action = "add"} = {}) { return action; }
 
-function calculateSingleGeneratorIncome(idle_rules_JSON, idle_stats_JSON, nameGenerator) {
-	const baseIncome_Array = json_getKey(idle_rules_JSON, `generators.${nameGenerator}.effects`);
+function calculateSingleGeneratorIncome(idle_rules_JSON, idle_stats_JSON, nameGenerator, action = "add") {
+// 	console.log("calculateSingleGeneratorIncome")
+	const baseIncome_Array = json_getKey(idle_rules_JSON, `generators.${nameGenerator}.effects`).filter( el => el.action == action);
+// 	console.log("baseIncome_Array", baseIncome_Array)
 	const generatorsQuantity = json_getKey(idle_stats_JSON, `generators.${nameGenerator}.quantity`);
-	
+// 	console.log("generatorsQuantity", generatorsQuantity)
 	const arrayNameCurrencies = getArrayNameCurrencies(idle_rules_JSON);
+// 	console.log("arrayNameCurrencies", arrayNameCurrencies)
 	const temporaryBankCurrencies = createTemporaryObject(arrayNameCurrencies);
-	
-	baseIncome_Array.forEach( i => temporaryBankCurrencies[i.currency] += calculateIncome(generatorsQuantity, i) );
+// 	console.log("temporaryBankCurrencies", temporaryBankCurrencies)
+	if (baseIncome_Array.length > 0 ) {
+		baseIncome_Array.forEach( i => temporaryBankCurrencies[i.currency] += calculateIncome(generatorsQuantity, i) );
+	}
 	return temporaryBankCurrencies;
 }
+
+// function calculateSingleGeneratorMultiplier(idle_rules_JSON, idle_stats_JSON, nameGenerator, action = "multiple") {
+// 	const baseMultipliers_Array = json_getKey(idle_rules_JSON, `generators.${nameGenerator}.effects`).filter( el => el.action == action);
+// 	const generatorsMultiplier = json_getKey(idle_stats_JSON, `generators.${nameGenerator}.multiplier_income`);
+		
+// 	const arrayNameGenerators = getArrayNameGenerators(idle_rules_JSON);
+// 	const temporaryStoreGenerators = createTemporaryObject(arrayNameGenerators);
+	
+// 	if (baseMultipliers_Array.length > 0 ) {
+// 		baseMultipliers_Array.forEach( i => temporaryStoreGenerators[i.generator] += calculateIncome(generatorsMultiplier, i) );
+// 	}
+// 	return temporaryStoreGenerators;
+// }
 
 function calculateTotalGeneratorIncome(idle_rules_JSON, idle_stats_JSON) {
 	const arrayNameGenerators = getArrayNameGenerators(idle_rules_JSON);
 	const arrayNameCurrencies = getArrayNameCurrencies(idle_rules_JSON);
 
-	const temporaryBankCurrencies = createTemporaryObject(arrayNameCurrencies);
-	const temporaryStoreGenerators = createTemporaryObject(arrayNameGenerators);
+	let temporaryBankCurrencies = createTemporaryObject(arrayNameCurrencies);
+	console.log("----------------------------------------");
+	console.log(temporaryBankCurrencies);
+	console.log("----------------------------------------");
+// 	const temporaryStoreGenerators = createTemporaryObject(arrayNameGenerators);
 	
-	// TO DO
-	arrayNameGenerators.forEach( nameGenerator => console.log(calculateSingleGeneratorIncome(idle_rules_JSON, idle_stats_JSON, nameGenerator)))
+	arrayNameGenerators.forEach( nameGenerator => {
+		
+		const effects =json_getKey(idle_rules_JSON, `generators.${nameGenerator}.effects`);
+		const action = effects[0].action;
+		
+		if (action == "multiple") {
+			const singleGeneratorIncome = calculateSingleGeneratorIncome(idle_rules_JSON, idle_stats_JSON, nameGenerator, action);
+			console.log("MULTIPLE", singleGeneratorIncome);
+		}
+	});
+	
+	console.log("----------------------------------------");
+	
+	arrayNameGenerators.forEach( nameGenerator => {
+		const effects =json_getKey(idle_rules_JSON, `generators.${nameGenerator}.effects`);
+		const action = effects[0].action;
+		if (action == "add"){
+			const singleGeneratorIncome = calculateSingleGeneratorIncome(idle_rules_JSON, idle_stats_JSON, nameGenerator, action);
+			console.log("ADD", singleGeneratorIncome);
+			const generatorMultiplicator =  json_getKey(idle_stats_JSON, `generators.${nameGenerator}.multiplier_income`);
+			const income = addObjPropertyToProperty(temporaryBankCurrencies, singleGeneratorIncome);
+			temporaryBankCurrencies = multipleObjPropertyToProperty(income, generatorMultiplicator);
+		}
+	});
+	console.log("end addObjPropertyToProperty");
+	console.log(temporaryBankCurrencies);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
