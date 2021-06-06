c9c7edd9f1912d794c291756c4ff790ab55bb1de
Samuele De Tomasi <el3um4s@gmail.com>
2021-06-07 00:45:12 +0200

    updated main.js (petits chevaux)

--- template/020-petits-chevaux/sources/files/scripts/main.js
+++ template/020-petits-chevaux/sources/files/scripts/main.js
@@ -1,14 +1,7 @@
 import { attachListeners } from "./postMessage.js";
 
-runOnStartup(async  runtime =>  {  globalThis.g_runtime  =  runtime })
-runOnStartup(async runtime =>
-{
+runOnStartup(async runtime => {
 	globalThis.g_runtime  =  runtime;
-	
 	runtime.addEventListener("beforeprojectstart", () => attachListeners());
 });
 
-// async function OnBeforeProjectStart(runtime)
-// {
-// 	attachListeners();
-// }
